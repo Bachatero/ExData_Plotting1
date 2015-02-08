fdb3b8d2206b6881821155b7b31c5e387376020b
Bachatero <queenmother@orangemail.sk>
2015-02-08 14:56:55 +0100

    Import plot1.R

---
+++ plot1.R
@@ -0,0 +1,11 @@
+setwd("/Users/oracle/datasciencecoursera/ExData_Plotting1/ExData_Plotting1")
+#fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+#download.file(fileUrl,destfile="./power_consumption.zip", method="curl")
+#library(sqldf)
+#egrep "^[1,2]/2/2007" household_power_consumption.txt
+data <- read.csv.sql("household_power_consumption.txt", header=TRUE, sep=";", sql="Select * from file where Date = '1/2/2007' OR Date = '2/2/2007'")
+
+#hist(data$Global_active_power)
+png(filename = "plot1.png", width = 480, height = 480,units = "px", pointsize = 12, bg = "white")
+hist(data$Global_active_power,col="Red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
+dev.off()
