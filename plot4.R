474b6a9d2c9dc6bae390141038187dc3d3b6cdea
Bachatero <queenmother@orangemail.sk>
2015-02-08 17:31:41 +0100

    Import plot4 files

---
+++ plot4.R
@@ -0,0 +1,36 @@
+#setwd("/Users/oracle/datasciencecoursera/ExData_Plotting1/ExData_Plotting1")
+setwd("~/ExData_Plotting1")
+
+library(sqldf)
+data <- read.csv.sql("household_power_consumption.txt", header=TRUE, sep=";", sql="Select * from file where Date = '1/2/2007' OR Date = '2/2/2007'")
+
+# combine date & time and convert into posix date/time value
+datetime<-strptime(paste(data$Date,data$Time), "%d/%m/%Y %H:%M:%S")
+
+# open png device
+png(filename = "plot4.png", width = 480, height = 480,units = "px", pointsize = 12, bg = "white")
+
+# set canvas for 4 plots
+par(mfrow=c(2,2))
+
+# create 1st plot: Global Active Power
+plot(datetime,data$Global_active_power,type="l",xlab="",ylab="Global Active Power")
+
+# create 2nd plot: Voltage
+plot(datetime,data$Voltage,type="l",xlab="datetime",ylab="Voltage")
+
+# create 3d plot: Enery sub metering # reuse plot3.R code
+plot(datetime, data$Sub_metering_1, type="n", xlab="", ylab="Energy sub metering")
+lines(datetime,data$Sub_metering_1,col="black")
+lines(datetime,data$Sub_metering_2,col="red")
+lines(datetime,data$Sub_metering_3,col="blue")
+legend_colors<-c("black","red","blue")
+legend_text<-c("Sub_metering_1","Sub_metering_2","Sub_metering_3")
+legend("topright",legend=legend_text, col=legend_colors,pch="-",lty=1)
+
+# create 4th plot: Global_reactive_power
+plot(datetime,data$Global_reactive_power,type="l",xlab="datetime",ylab="Global_reactive_power")
+
+dev.off()
+
+
